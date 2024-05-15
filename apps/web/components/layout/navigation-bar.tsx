1593b07d82450617af18694498146ead35029d7b
Jonas Scholz <Jonas.Scholz@bbscholz.de>
2024-05-15 02:24:17 +0200

    fix: remove placeholder

--- apps/web/components/layout/navigation-bar.tsx
+++ apps/web/components/layout/navigation-bar.tsx
@@ -27,21 +27,6 @@ export default function NavigationBar({
     <header className="sticky top-0 z-10 w-full border-b bg-white">
       <nav className="mx-auto flex h-16 max-w-7xl flex-row items-center justify-around p-6 lg:gap-x-12 lg:px-8">
         <span className="text-2xl font-bold lg:justify-start">InsightOut</span>
-
-        <div className="flex flex-row gap-x-2">
-          <Input
-            value={searchInput}
-            onChange={(e) => setSearchInput(e.target.value)}
-            placeholder="AI Search"
-          />
-          <Button
-            onClick={() => {
-              console.log("searching for", searchInput);
-            }}
-          >
-            <span className="text-md text-base font-medium">Search</span>
-          </Button>
-        </div>
         <div className="lg:flex lg:flex-1 lg:justify-end">
           {user ? (
             <AvatarMenu username={user.email} />
