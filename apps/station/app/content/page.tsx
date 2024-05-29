e510e8b8ff1e3189310ff065efd461df79b2d055
Jonas Scholz <Jonas.Scholz@bbscholz.de>
2024-05-29 01:30:10 +0100

    feat: qr code login

--- apps/station/app/content/page.tsx
+++ apps/station/app/content/page.tsx
@@ -1,6 +1,8 @@
-import { prisma } from "database";
 import { Text } from "./text";
 import Record from "./record";
+import { MoveRightIcon } from "lucide-react";
+import Link from "next/link";
+import { prisma } from "database";
 
 export default async function Page() {
   const question = await prisma.post.findFirst({
@@ -10,11 +12,17 @@ export default async function Page() {
   });
 
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-between bg-black p-10">
+    <Link
+      className="flex h-screen w-screen flex-col items-center justify-between bg-black p-10"
+      href="/end"
+    >
       <div className="mx-auto text-center leading-10">
         <Text text={question.content} />
       </div>
       <Record />
-    </div>
+      <div className="absolute bottom-8 flex w-screen animate-pulse flex-row items-center justify-center gap-4 text-center align-middle font-mono text-xl font-bold text-white">
+        Touch anywhere to end <MoveRightIcon className="h-8 w-8" />
+      </div>
+    </Link>
   );
 }
---
+++ apps/station/app/end/page.tsx
@@ -0,0 +1,42 @@
+import { MoveRightIcon } from "lucide-react";
+import QRCode from "react-qr-code";
+import randomAnimalName from "random-animal-name";
+import Link from "next/link";
+
+async function createAnonymousUser() {
+  const randomAnimal = randomAnimalName();
+  const slug = randomAnimal.toLowerCase().replaceAll(" ", "-");
+  const email = `anonymous-${slug}@prototype.sliplane.app`;
+
+  const password = generateRandomPassword();
+
+  return {
+    email,
+    password,
+  };
+}
+
+function generateRandomPassword() {
+  return (
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15)
+  );
+}
+
+export default async function Page() {
+  const user = await createAnonymousUser();
+  const qrCode = `https://prototype.sliplane.app/auth/signin?email=${user.email}&token=${user.password}&signup=true`;
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-8 bg-black">
+      <div className="flex w-screen flex-row items-center justify-center gap-4 text-center align-middle font-mono text-xl font-bold text-white">
+        Join our platform! <br />
+      </div>
+      <Link href={qrCode} target="_blank">
+        <QRCode value={qrCode} bgColor="#000" fgColor="#FFA500" />
+      </Link>
+      <div className="flex w-screen flex-row items-center justify-center gap-4 text-center align-middle font-mono text-xl font-bold text-white">
+        Touch anywhere to start again <MoveRightIcon className="h-8 w-8" />
+      </div>
+    </div>
+  );
+}
